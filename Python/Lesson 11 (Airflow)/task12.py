a1c50ab267ca31d280d0589c61f370d2c0e0e86f
Daria Stikheeva <dstikheeva@yandex.ru>
2024-10-15 15:13:35 +0300

    add task 12

---
+++ Python/Lesson 11 (Airflow)/task12.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable
+
+with DAG(
+    "hw_darja-stiheeva-lms4973_11",
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    description="A DAG for homework",
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2024, 10, 14),
+    catchup=False,
+) as dag:
+    def print_var():
+        print(Variable.get("is_startml"))
+
+    t = PythonOperator(
+        task_id='print_variable',
+        python_callable=print_var
+    )
