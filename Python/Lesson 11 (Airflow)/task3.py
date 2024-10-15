cf727d223187e4bc451bb233bf2bdc018463bb69
Daria Stikheeva <dstikheeva@yandex.ru>
2024-10-15 15:08:55 +0300

    add task 3

---
+++ Python/Lesson 11 (Airflow)/task3.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    "hw_darja-stiheeva-lms4973_3",
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    description="A DAG for homework",
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2024, 10, 14),
+    catchup=False,
+) as dag:
+    for i in range(10):
+        t = BashOperator(
+            task_id=f'dag_bash_{i}',
+            bash_command=f'echo {i}',
+        )
+
+    def print_task_num(task_number):
+        print(f'task number is: {task_number}')
+
+    for i in range(20):
+        t = PythonOperator(
+            task_id=f'dag_python_{10 + i}',
+        python_callable=print_task_num,
+        op_kwargs={'task_number': 10 + i}
+        )
